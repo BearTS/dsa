c91382ffdef28bb32430d5398a9ead9c19dcd519
Anuj Parihar <anujparihar@yahoo.com>
2022-11-16 12:50:30 +0530

    Update Binary Search.cpp

--- DSA Lab Fat/PS 3/Binary Search.cpp
+++ DSA Lab Fat/PS 3/Binary Search.cpp
@@ -1,35 +1,62 @@
-// binary searh
-#include <iostream>
-using namespace std;
-
-int main()
+int binarySearch(int arr[], int l, int r, int x)
 {
-    int n;
-    int *arr;
-    cout << "Enter number of digits to be sorted: ";
-    cin >> n;
-    arr = new int[n];
-    cout << "Enter the Digits: " << endl;
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    int low = 0, mid, key;
-    int high = n - 1;
-    cout << "Enter the key to be searched: ";
-    cin >> key;
-    while (low <= high)
-    {
-        mid = (low + high) / 2;
-        if (key == arr[mid])
-        {
-            cout << "Key found at position " << mid + 1 << endl;
-            break;
-        }
-        else if (key < arr[mid])
-            high = mid - 1;
-        else
-            low = mid + 1;
+
+    if (r >= l) {
+
+        int mid = l + (r - l) / 2;
+ 
+
+        // If the element is present at the middle
+
+        // itself
+
+        if (arr[mid] == x)
+
+            return mid;
+ 
+
+        // If element is smaller than mid, then
+
+        // it can only be present in left subarray
+
+        if (arr[mid] > x)
+
+            return binarySearch(arr, l, mid - 1, x);
+ 
+
+        // Else the element can only be present
+
+        // in right subarray
+
+        return binarySearch(arr, mid + 1, r, x);
+
     }
-    if (low > high)
-        cout << "Key not found" << endl;
+ 
+
+    // We reach here when element is not
+
+    // present in array
+
+    return -1;
+}
+ 
+
+int main(void)
+{
+
+    int arr[] = { 2, 3, 4, 10, 40 };
+
+    int x = 10;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = binarySearch(arr, 0, n - 1, x);
+
+    (result == -1)
+
+        ? cout << "Element is not present in array"
+
+        : cout << "Element is present at index " << result;
+
     return 0;
 }
